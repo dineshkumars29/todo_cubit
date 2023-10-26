f735c562bb91c86f0d3287b777f9c0628711f753
dinesh kumar <dineshdeveloper29@gmail.com>
2023-10-26 14:33:18 +0530

    setup

---
+++ lib/cubits/todo_filter/todo_filter_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:todocubit/models/todo_model.dart';
+
+part 'todo_filter_state.dart';
+
+class TodoFilterCubit extends Cubit<TodoFilterState> {
+  TodoFilterCubit() : super(TodoFilterState.initial());
+}
